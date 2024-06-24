a21fe2372d44ed6206603e9036cad92a24a5f7c5
leslientodi <lesliehazard500@gmail.com>
2024-06-24 10:17:31 +0000

    set programmes for all ones

--- career-dev/src/components/AllOnes.js
+++ career-dev/src/components/AllOnes.js
@@ -3,10 +3,51 @@ import React from "react";
 
 
 export default function AllOnes(){
+    const [career, setCareer] = React.useState(
+        {field:""}
+    )
+    const [degree, setDegree] = React.useState("")
+
+    
+
+
+    function handleInput(event) {
+        const {name, value} = event.target
+        setCareer(prev => {
+          return {
+            ...prev,
+            [name]:  value
+          }
+        })
+        
+        
+      }
+
+      function selectField(){
+        console.log(career.field)
+        if(career.field === "Human Anatomy"){
+            setDegree(<h2>Bsc Human Biology(Medicine) </h2>)
+        } else if (career.field === "Organic Chemistry"){
+            setDegree(<h2>Bsc Pharmacy</h2>)
+        } else if (career.field === "Lab Equipments"){
+            setDegree(<h2>Bsc Biomedical Engineering</h2>)
+        }else if(career.field === "Heat and Mechanics"){
+            setDegree(<h2>Bsc Mechanical Engineering</h2>)
+        } else if (career.field === "Electricity"){
+            setDegree(<h2>Bsc Electrical Engineering</h2>)
+        } else if (career.field === "Strength Of Materials"){
+            setDegree(<h2>Bsc Civil Engineering</h2>)
+        } else if (career.field === "Thermodynamics"){
+            setDegree(<h2>Bsc Aerospace Engineering</h2>)
+        }
+      }
+
+
     return(
         <>
             <h2>Select your field of interest</h2>
-                <select class="fields-HM">
+                <select id="field" value={career.field} onChange={handleInput} name="field">
+                    <option>-- Choose field --</option>
                    <option>Heat and Mechanics</option>
                    <option>Electricity</option>
                    <option>Strength Of Materials</option>
@@ -21,7 +62,8 @@ export default function AllOnes(){
                 </select>
                 <button class="submit"
                 
-                >Submit</button>
+                onClick={selectField}>Submit</button>
+                {degree}
         </>
     )
 }
