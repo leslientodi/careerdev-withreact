b7c144b573f9d7fe9fe49b5aca8beac487dad703
leslientodi <lesliehazard500@gmail.com>
2024-06-24 11:08:57 +0000

    set programmes for medics fields

--- career-dev/src/components/Medics.js
+++ career-dev/src/components/Medics.js
@@ -1,31 +1,65 @@
 import React from "react";
 
+export default function Medics() {
+  const [career, setCareer] = React.useState({ field: "" });
+  const [degree, setDegree] = React.useState("");
 
+  function handleInput(event) {
+    const { name, value } = event.target;
+    setCareer((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
 
-export default function Medics(){
-    return (
+  function selectField() {
+    console.log(career.field);
+    if (career.field === "Human Eye") {
+      setDegree(<h2>Bsc Optometry</h2>);
+    } else if (career.field === "Dentition") {
+      setDegree(<h2>Bsc Dentistry</h2>);
+    } else if (career.field === "Animal life") {
+      setDegree(<h2>Bsc Vertinary</h2>);
+    } else if (career.field === "Plant Anatomy") {
+      setDegree(<h2>Bsc Horticulture</h2>);
+    } else if (career.field === "The skin") {
+      setDegree(<h2>Bsc Dermatology</h2>);
+    } else if (career.field === "The brain and cognitive abilities") {
+      setDegree(<h2>Bsc Psychology</h2>);
+    } else if (career.field === "Food and Nutrition") {
+      setDegree(
         <>
-            <h2>Select your field of interest</h2>
-        <select class="fields-HM">
-           
-           <option>Human Eye</option>
-           <option>Dentition</option>
-           <option>Animal life</option>
-           <option>Plant Anatomy</option>
-           <option>The skin</option>
-           <option>The brain and cognitive abilities</option>
-           <option>Food and Nutrition</option>
-           
-
-           
-        
-        
-        
-        
-        </select>
-        <button class="submit"
-        
-        >Submit</button>
+          <h2>Bsc Food Science</h2>
+          <h2>Bsc Dietetics</h2>
         </>
-    )
+      );
+    }
+  }
+
+  return (
+    <>
+      <h2>Select your field of interest</h2>
+      <select
+        id="field"
+        value={career.field}
+        onChange={handleInput}
+        name="field"
+      >
+        <option>-- Choose field --</option>
+        <option>Human Eye</option>
+        <option>Dentition</option>
+        <option>Animal life</option>
+        <option>Plant Anatomy</option>
+        <option>The skin</option>
+        <option>The brain and cognitive abilities</option>
+        <option>Food and Nutrition</option>
+      </select>
+      <button class="submit" onClick={selectField}>
+        Submit
+      </button>
+      {degree}
+    </>
+  );
 }
