1e2e537839605db63a72b53a023607211155fd14
leslientodi <lesliehazard500@gmail.com>
2024-06-24 11:14:13 +0000

    set programmes for math fields

--- career-dev/src/components/Math.js
+++ career-dev/src/components/Math.js
@@ -1,25 +1,45 @@
 import React from "react";
 
+export default function Math() {
+  const [career, setCareer] = React.useState({ field: "" });
+  const [degree, setDegree] = React.useState("");
 
-export default function Math (){
-    return(
-        <>
-            <h2>Select your field of interest</h2>
-        <select class="fields-HM">
-           
-           <option>Statistics</option>
-           <option>General Mathematics</option>
-           
+  function handleInput(event) {
+    const { name, value } = event.target;
+    setCareer((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
 
-           
-        
-        
-        
-        
-        </select>
-        <button class="submit"
-        
-        >Submit</button>
-        </>
-    )
+  function selectField() {
+    console.log(career.field);
+    if (career.field === "Statistics") {
+      setDegree(<h2>Bsc Statistics</h2>);
+    } else if (career.field === "General Mathematics") {
+      setDegree(<h2>Bsc Mathematics</h2>);
+    }
+  }
+
+  return (
+    <>
+      <h2>Select your field of interest</h2>
+      <select
+        id="field"
+        value={career.field}
+        onChange={handleInput}
+        name="field"
+      >
+        <option>-- Choose field --</option>
+        <option>Statistics</option>
+        <option>General Mathematics</option>
+      </select>
+      <button class="submit" onClick={selectField}>
+        Submit
+      </button>
+      {degree}
+    </>
+  );
 }
